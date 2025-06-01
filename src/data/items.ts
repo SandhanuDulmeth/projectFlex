65e6d93755478ecf63e5378926545a9c0a96621c
sandhanu <sandhanu12@gmail.com>
2025-06-01 12:05:04 +0530

    feat: add new Facebook post items to shopItems data
    - Introduced additional shop items with iframe URLs for Facebook posts, enhancing content variety and user engagement.
    - Each new item includes an ID, iframe URL, and dimensions for consistent display.

--- src/data/items.ts
+++ src/data/items.ts
@@ -18,6 +18,30 @@ export const shopItems: ShopItem[] = [
     iframeUrl: 'https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fpermalink.php%3Fstory_fbid%3Dpfbid0kU2pv5Lz39TBcmAUZHi6QbRQXjUFcMxUBUAmobyMh6Q3tDCc4Ea2DVp3QKwpCB9bl%26id%3D61576760342310&show_text=true&width=500',
     width: 500,
     height: 674
+  },
+  {
+    id: '4',
+    iframeUrl: 'https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fpermalink.php%3Fstory_fbid%3Dpfbid05mscEDf58efaa858tKQdWgeUYGfffHwJ7PnMoUjaDfz59ZesxCsYzAe1a3cFqDWul%26id%3D61576760342310&show_text=true&width=500',
+    width: 500,
+    height: 674
+  },
+  {
+    id: '5',
+    iframeUrl: 'https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fpermalink.php%3Fstory_fbid%3Dpfbid0Uw3mLq9D9iPXqfY3w7VGcpxjbJpnZV8o9dgkKTv9SPgoVCeSL5d8s6sf1c5uD6MJl%26id%3D61576760342310&show_text=true&width=500',
+    width: 500,
+    height: 674
+  },
+  {
+    id: '6',
+    iframeUrl: 'https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fpermalink.php%3Fstory_fbid%3Dpfbid029V8FhgyoMjxfV1Qqzj8QVVCF3eWx91gatEj6XPUEU4ruaTzSthKHv8FjSV5bcZ5Fl%26id%3D61576760342310&show_text=true&width=500',
+    width: 500,
+    height: 674
+  },
+  {
+    id: '7',
+    iframeUrl: 'https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fpermalink.php%3Fstory_fbid%3Dpfbid02oYhViRC7REgvxUwywt1y2Cxbbtk2rg6oThZaV8qcXALoDzqC5Lr5hButC3SDALQcl%26id%3D61576760342310&show_text=true&width=500',
+    width: 500,
+    height: 674
   }
 
 ];
